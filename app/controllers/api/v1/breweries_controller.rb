e3dbc165621e804c6cb4fe44f5fa5094aca6e1ab
Jesus Escalona <jesusescalona@me.com>
2019-05-13 20:57:52 -0400

    api done

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -1,19 +1,29 @@
-class Api::V1::BreweriesController < ApplicationController
+# frozen_string_literal: true
 
-  def index
-    @breweries = Brewery.all.limit(20)
-    render json: @breweries
+module Api
+  # frozen_string_literal: true
+
+  module V1
+    # :nodoc:
+    class BreweriesController < ApplicationController
+
+      def index
+        @breweries = Brewery.all.limit(20)
+        render json: @breweries
+      end
+
+      def show
+        @brewery = Brewery.find(params[:id])
+        render json: @brewery
+      end
+
+      def by_city
+        @city = params[:by_city]
+        @brewery = Brewery.where('lower(city) LIKE ?', "%#{@city.downcase}%")
+        render json: @brewery
+      end
+
+    end
   end
-
-  def show
-    @brewery = Brewery.find(params[:id])
-    render json: @brewery
-  end
-
-  def by_city
-    @city = params[:by_city]
-    @brewery = Brewery.where('lower(city) LIKE ?', "%#{@city.downcase}%")
-    render json: @brewery
-  end
-
 end
+
