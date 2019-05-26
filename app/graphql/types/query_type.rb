7a27cd736f4cb12918e26ac744614e8f7c6fb600
Jesus Escalona <jesusescalona@me.com>
2019-05-26 15:12:56 -0400

    implemented GraphQL queries on api

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,12 +4,27 @@ module Types
     # They will be entry points for queries on your schema.
     field :all_breweries, [BreweryType], null: false
     field :some_breweries, [BreweryType], null: false
+    field :brewery, [BreweryType], null: false do
+      argument :id, ID, required: true
+    end
+    field :breweries_by_city, [BreweryType], null: false do
+      argument :city, String, required: true
+    end
+
     def all_breweries
       Brewery.all
     end
 
     def some_breweries
       Brewery.all.limit(5)
+    end
+
+    def brewery(id:)
+      Brewery.find(id)
+    end
+
+    def breweries_by_city(city:)
+      Brewery.where('lower(city) LIKE ?', city.downcase)
     end
   end
 end
